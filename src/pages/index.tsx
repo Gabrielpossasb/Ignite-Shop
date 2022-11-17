184605a3d72aa7d9e615df1d9aff62792b3bee64
Gabriel Possas <gabrielpossas2014@gmail.com>
2022-11-16 21:48:05 -0400

    1.0

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { styled } from '../../styles'
+
+const Button222 = styled('button', {
+  backgroundColor: '$green300',
+  
+})
+
+export default function Home() {
+  return (
+    <Button222>
+      oasfasssssss
+    </Button222>
+  )
+}
