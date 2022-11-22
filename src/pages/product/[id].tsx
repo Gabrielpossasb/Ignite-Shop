0bf9db815d62b22e6650585acf21a4a2ee628b5b
Gabriel Possas <gabrielpossas2014@gmail.com>
2022-11-22 16:03:29 -0400

    stripe-success-buy

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,7 @@
+import axios from "axios";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useState } from "react";
 import Stripe from "stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "../../../styles/pages/product";
 import { stripe } from "../../lib/stripe";
@@ -12,10 +13,30 @@ interface ProductProps {
       imageUrl: string;
       price: string;
       description: string;
+      defaultPriceId: string;
    }
 }
 
 export default function Product({ product }: ProductProps) {
+   const [ isCreatingCheckoutSession, setIsCreatingCheckoutSession ] = useState(false)
+
+   async function handleBuyProduct() {
+      try {
+         setIsCreatingCheckoutSession(true)
+
+         const response = await axios.post('/api/checkout', {
+            priceId: product.defaultPriceId,
+         })
+
+         const { checkoutUrl } = response.data;
+
+         window.location.href = checkoutUrl
+      } catch (err) {
+         setIsCreatingCheckoutSession(false)
+         alert('Falha ao redirecionar ao checkout!')
+      }
+   }
+
    return (
       <ProductContainer>
          <ImageContainer>
@@ -28,7 +49,7 @@ export default function Product({ product }: ProductProps) {
 
             <p>{product.description}</p>
 
-            <button>
+            <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
                Comprar agora
             </button>
          </ProductDetails>
@@ -38,14 +59,12 @@ export default function Product({ product }: ProductProps) {
 
 export const getStaticPaths: GetStaticPaths = async () => {
    return {
-      paths: [
-         { params: { id: '' } }
-      ],
+      paths: [],
       fallback: "blocking",
    }
 }
 
-export const getStaticPropa: GetStaticProps<any, { id: string }> = async ({ params }) => {
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
    const productId = params.id;
 
    const product = await stripe.products.retrieve(productId, {
@@ -65,6 +84,7 @@ export const getStaticPropa: GetStaticProps<any, { id: string }> = async ({ para
                currency: 'BRL',
             }).format( price.unit_amount / 100 ),
             description: product.description,
+            defaultPriceId: price.id,
          }
       },
       revalidate: 60 * 60 * 1, // 1 hour
